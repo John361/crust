a87f95850ac99abd29a24324e1350b20ec2f6622
John H <hxjwork1@gmail.com>
2024-09-18 22:46:10 +0400

    Add second implementation to Schedule to avoid complicated management, wrap multiple Task into Tasks

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 mod task;
 
-use crate::task::{Schedule, Task};
+use crate::task::{Schedule, Task, Tasks};
 
 fn main() {
-    let schedule1 = Schedule { minute: 9, hour: 22 };
-    let schedule2 = Schedule { minute: 10, hour: 22 };
+    let schedule1 = Schedule { hour: 22, minute: 44, second: 30 };
+    let schedule2 = Schedule { hour: 22, minute: 44, second: 35 };
 
     let task1 = Task { schedule: schedule1, command: "hello".to_string() };
     let task2 = Task { schedule: schedule2, command: "world".to_string() };
-    let tasks = vec![task1, task2];
+    let tasks = Tasks { tasks: vec![task1, task2] };
 
     loop {
-        for task in &tasks {
-            task.execute();
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        for task in &tasks.tasks {
+            task.execute(); // TODO: open new thread for execution
         }
     }
 }
--- src/task.rs
+++ src/task.rs
@@ -2,37 +2,34 @@ use chrono::{DateTime, Local, Timelike};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+pub struct Tasks {
+    pub tasks: Vec<Task>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
 pub struct Task {
     pub schedule: Schedule,
     pub command: String,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Schedule {
-    pub minute: u32,
-    pub hour: u32,
-}
-
 impl Task {
     pub fn execute(&self) {
-        if self.can_be_activated() && self.schedule.is_time() {
+        if self.schedule.is_time() {
             println!("{}", self.command);
         }
     }
+}
 
-    fn can_be_activated(&self) -> bool {
-        !self.schedule.is_time_pasted()
-    }
+#[derive(Clone, Debug, Deserialize)]
+pub struct Schedule {
+    pub hour: u32,
+    pub minute: u32,
+    pub second: u32,
 }
 
 impl Schedule {
     pub fn is_time(&self) -> bool {
         let now: DateTime<Local> = Local::now();
-        now.hour() == self.hour && now.minute() == self.minute
-    }
-
-    pub fn is_time_pasted(&self) -> bool {
-        let now: DateTime<Local> = Local::now();
-        now.hour() > self.hour && now.minute() > self.minute
+        now.hour() == self.hour && now.minute() == self.minute && now.second() == self.second
     }
 }
