39d0685984acd3b2745e10ee6830e10392dce321
John H <hxjwork1@gmail.com>
2024-09-18 23:23:39 +0400

    Handle task status and print message

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use crust_lib::config::CrustConfig;
+use crust_lib::task::TaskStatus;
 
 fn main() -> anyhow::Result<()> {
     let config = CrustConfig::load()?;
@@ -7,7 +8,18 @@ fn main() -> anyhow::Result<()> {
         std::thread::sleep(std::time::Duration::from_secs(1));
 
         for task in &config.tasks {
-            task.execute()?; // TODO: open new thread for execution
+            let result = task.execute()?; // TODO: open new thread for execution
+            match result {
+                TaskStatus::Success => {
+                    println!("Task successfully executed.");
+                }
+
+                TaskStatus::Error(error) => {
+                    println!("Task execution error: {}", error);
+                }
+
+                TaskStatus::NotReady => {}
+            }
         }
     }
 }
